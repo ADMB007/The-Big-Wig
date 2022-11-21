768a9461dcb42b3fe1573fb45d80cbc6b7763ccf
Avery Millican-Bowers <105184089+ADMB007@users.noreply.github.com>
2022-11-21 01:05:45 -0600

    Event Calendar
    Event calendar code and mock format

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import { Login } from './pages/Login';
-import { Home } from './pages/Home';
-import { ExamplePortfolio } from './pages/ExamplePortfolio';
-import { Signup } from './pages/Signup';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/Login' element={<Login />} />
-      <Route path='/Signup' element={<Signup />} />
-      <Route path='/ExamplePortfolio' element={<ExamplePortfolio />} />
-      <Route path='/Events' element={<Home />} />
-      <Route path='/Tutorials' element={<Home />} />
-    </Routes>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
-
 
 export default App;
